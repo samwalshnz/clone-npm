e987d6bb0ab00fd92c4da6b62d36e61c7863186d
Sam Walsh <Sam.Walsh@serko.com>
2018-07-27 11:00:39 +1200

    Add bin command

---
+++ bin/clone.js
@@ -0,0 +1,7 @@
+#! /usr/bin/env node
+const {
+    getPackageNameFromCommand,
+    clonePackage,
+} = require('../index')
+
+clonePackage()
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+
 var git = require('simple-git');
 var packageJson = require('package-json');
-var packageName = process.argv[2];
+
+const getPackageNameFromCommand = function() {
+    return process.argv[2]
+}
 
 const getOptions = function() {
     return process.argv.slice(3);
@@ -38,6 +42,21 @@ const stdout = function(cloning) {
     });
 }
 
-packageJson(packageName, {fullMetadata: true})
+const clonePackage = function(packageName, options) {
+    packageName || (packageName = getPackageNameFromCommand());
+
+    if (! packageName) {
+        console.error(`\nMissing npm package name. \n(e.g. \`npm-clone react\`)`);
+        process.exit();
+    }
+
+    packageJson(packageName, {fullMetadata: true})
         .then(getRepoUrl)
         .then(clone)
+};
+
+module.exports = {
+    clonePackage,
+    getOptions,
+    getPackageNameFromCommand,
+};
